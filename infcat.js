7bd4310ed9836a9626285ffb1ee9d94de2f2bfda
zyme <lolrichyisswag@gmail.com>
2020-12-14 10:50:06 -0600

    I GENERATORED FREE CATERS (OBAMA ILLEGAL???)

--- infcat.js
+++ infcat.js
@@ -1,15 +1,18 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
 async function catHell() {
-    let resp = await fetch(`http://shibe.online/api/cats?count=$100&urls=false&httpsUrls=true`) // do a request
-    let FileNames = await resp.json()
-    let downloads = FileNames.map(async function (FileName) {
-        let response2 = await fetch('https://cdn.shibe.online/cats/' + FileName + '.jpg')
-        let writestream = fs.createWriteStream('./folder/' + FileName + '.jpg')
-        response2.body.pipe(writestream)
-    })
-    await Promise.all(downloads)
+    try {
+        let resp = await fetch(`http://shibe.online/api/cats?count=$100&urls=false&httpsUrls=true`) // do a request
+        let FileNames = await resp.json()
+        let downloads = FileNames.map(async function (FileName) {
+            let response2 = await fetch('https://cdn.shibe.online/cats/' + FileName + '.jpg')
+            let writestream = fs.createWriteStream('./folder/' + FileName + '.jpg')
+            response2.body.pipe(writestream)
+            await Promise.all(downloads)
+        })
+    } catch (e) {
+        console.error();
+    }
     catHell()
 }
-// cat hell
 catHell()
