501c190a0b14e2372d18bab2976c5c8631f64789
zyme <lolrichyisswag@gmail.com>
2020-12-14 00:19:56 -0600

    option infinity cat why ???

---
+++ infcat.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch')
+const fs = require('fs')
+async function catHell() {
+    let resp = await fetch(`http://shibe.online/api/cats?count=$100&urls=false&httpsUrls=true`) // do a request
+    let FileNames = await resp.json()
+    let downloads = FileNames.map(async function (FileName) {
+        let response2 = await fetch('https://cdn.shibe.online/cats/' + FileName + '.jpg')
+        let writestream = fs.createWriteStream('./folder/' + FileName + '.jpg')
+        response2.body.pipe(writestream)
+
+
+    })
+    await Promise.all(downloads)
+    catHell()
+}
+
+catHell()
